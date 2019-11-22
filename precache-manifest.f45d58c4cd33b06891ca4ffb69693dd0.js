5fca8f611fec225ebbb515036da82c7e800c425f
Ava DeLaCruz <avad333@gmail.com>
2019-11-22 13:20:36 -0800

    Updates

---
+++ precache-manifest.f45d58c4cd33b06891ca4ffb69693dd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18b2995fc77a8cad7e9c65bc7bdebde3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd5302e6750d16097eaf",
+    "url": "/static/css/main.728c1e52.chunk.css"
+  },
+  {
+    "revision": "6cc2d9db497636aad010",
+    "url": "/static/js/2.e1162d1a.chunk.js"
+  },
+  {
+    "revision": "fd5302e6750d16097eaf",
+    "url": "/static/js/main.9920d20b.chunk.js"
+  },
+  {
+    "revision": "2bff719c5496e3cbc8a5",
+    "url": "/static/js/runtime-main.ba94b3aa.js"
+  }
+]);
