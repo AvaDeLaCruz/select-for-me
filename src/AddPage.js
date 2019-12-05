bcb8b3d4e25c70889262c85f474ebf8928a4c4ea
Ava DeLaCruz <avad333@gmail.com>
2019-12-04 16:37:07 -0800

    add loading feature and basic ajax call

--- src/AddPage.js
+++ src/AddPage.js
@@ -1,10 +1,29 @@
 import React from "react";
 import "./AddPage.css";
+import Loading from "./Loading";
+import "./Loading.css";
 
 export default class AddPage extends React.Component {
-  handleSearch = event => {
+  constructor() {
+    super();
+    this.state = {
+      results: [],
+      loading: false
+    };
+  }
+
+  handleSearch = async event => {
     event.preventDefault();
-    console.log(event.target.searchTerm.value);
+    this.setState({ loading: true });
+    let searchTerm = event.target.searchTerm.value;
+    let url =
+      "https://api.edamam.com/search?app_id=6a7eb714&app_key=2a86ad87280db39e7fb0fadd79840ecf&q=" +
+      searchTerm;
+    let response = await fetch(url);
+    let json = await response.json();
+    let results = json.hits;
+    console.log(results);
+    this.setState({ results, loading: false });
   };
 
   render() {
@@ -21,6 +40,13 @@ export default class AddPage extends React.Component {
           ></input>
           <input type="submit" className="searchButton" value="Search"></input>
         </form>
+        {this.state.loading ? (
+          <Loading />
+        ) : (
+          this.state.results.map(result => {
+            return <div key={result.recipe.uri}>{result.recipe.label}</div>;
+          })
+        )}
       </div>
     );
   }
---
+++ src/Loading.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Loading.css";
+
+export default function Loading() {
+  return (
+    <div className="loadingio-spinner-dual-ball-573md0tcy56">
+      <div className="ldio-jbaa0zm6k5p">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
