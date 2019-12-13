40f3b02e3f4e5e1a664a02b165afc3d56f9cbcfa
Ava DeLaCruz <avad333@gmail.com>
2019-12-12 23:26:03 -0800

    Updates

---
+++ precache-manifest.006e04741b6715fdcb50896435c812d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc67ecc30d644813db269a50d03d34e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfb1254050700dcc7e8c",
+    "url": "/static/css/main.32aeb669.chunk.css"
+  },
+  {
+    "revision": "44a718ba514a8c2f3ec6",
+    "url": "/static/js/2.e5644919.chunk.js"
+  },
+  {
+    "revision": "cfb1254050700dcc7e8c",
+    "url": "/static/js/main.71efb38e.chunk.js"
+  },
+  {
+    "revision": "2bff719c5496e3cbc8a5",
+    "url": "/static/js/runtime-main.ba94b3aa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cbe4b9b4d50329523f43bc579b204073.js"
+  "/precache-manifest.006e04741b6715fdcb50896435c812d7.js"
 );
 
 self.addEventListener('message', (event) => {
