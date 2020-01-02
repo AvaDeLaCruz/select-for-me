94b6718c9e0770eecf718f1ba4f86b7a31805a83
Ava DeLaCruz <avadelac@usc.edu>
2020-01-02 12:17:31 -0500

    update package, remove console logs

--- src/components/AddPage.js
+++ src/components/AddPage.js
@@ -7,161 +7,156 @@ import RecipeDetails from "./RecipeDetails";
 import DocumentTitle from "react-document-title";
 
 export default class AddPage extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			results: [],
-			loading: false,
-			userHasSearched: false,
-			detailLink: "",
-			db: []
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+      loading: false,
+      userHasSearched: false,
+      detailLink: "",
+      db: []
+    };
+  }
 
-	handleSearch = async event => {
-		event.preventDefault();
-		this.setState({ loading: true });
-		let searchTerm = event.target.searchTerm.value;
-		let url =
-			"https://api.edamam.com/search?app_id=6a7eb714&app_key=2a86ad87280db39e7fb0fadd79840ecf&q=" +
-			searchTerm;
-		let response = await fetch(url);
+  handleSearch = async event => {
+    event.preventDefault();
+    this.setState({ loading: true });
+    let searchTerm = event.target.searchTerm.value;
+    let url =
+      "https://api.edamam.com/search?app_id=6a7eb714&app_key=2a86ad87280db39e7fb0fadd79840ecf&q=" +
+      searchTerm;
+    let response = await fetch(url);
 
-		if (response.ok) {
-			console.log("good");
+    if (response.ok) {
+      let json = await response.json();
+      let results = json.hits;
+      this.props.history.push(`/add/${searchTerm}`);
+      this.setState({ results, userHasSearched: true });
+    } else {
+      document.getElementById("notif").classList.toggle("visible");
+      document.getElementById("notif").innerText =
+        "Error: Could not complete search.";
+      setTimeout(() => {
+        document.getElementById("notif").classList.toggle("visible");
+      }, 2500);
+    }
 
-			let json = await response.json();
-			let results = json.hits;
-			console.log(results);
-			this.props.history.push(`/add/${searchTerm}`);
-			this.setState({ results, userHasSearched: true });
-		} else {
-			console.log("other error");
-			document.getElementById("notif").classList.toggle("visible");
-			document.getElementById("notif").innerText =
-				"Error: Could not complete search.";
-			setTimeout(() => {
-				document.getElementById("notif").classList.toggle("visible");
-			}, 2500);
-		}
+    this.setState({ loading: false });
+  };
 
-		this.setState({ loading: false });
-	};
+  viewDetails = url => {
+    url === this.state.detailLink
+      ? this.setState({ detailView: false, detailLink: "" })
+      : this.setState({ detailView: true, detailLink: url });
+  };
 
-	viewDetails = url => {
-		url === this.state.detailLink
-			? this.setState({ detailView: false, detailLink: "" })
-			: this.setState({ detailView: true, detailLink: url });
-	};
+  favorite = async (title, author, servings, ingredients, directions, url) => {
+    let data = {
+      title: title,
+      author: author,
+      servings: servings,
+      ingredients: ingredients,
+      directions: directions,
+      url: url
+    };
 
-	favorite = async (title, author, servings, ingredients, directions, url) => {
-		let data = {
-			title: title,
-			author: author,
-			servings: servings,
-			ingredients: ingredients,
-			directions: directions,
-			url: url
-		};
+    let apiURL = "https://cooking-companion-api.herokuapp.com/recipes";
 
-		let apiURL = "https://cooking-companion-api.herokuapp.com/recipes";
+    const response = await fetch(apiURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
 
-		const response = await fetch(apiURL, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify(data)
-		});
+    document.getElementById("notif").classList.toggle("visible");
 
-		document.getElementById("notif").classList.toggle("visible");
+    if (response.ok) {
+      let json = await response.json();
+      document.getElementById("notif").innerText =
+        "Recipe successfully added to favorites. ";
+    } else {
+      document.getElementById("notif").innerText =
+        "Error: Could not add recipe to favorites. ";
+    }
 
-		if (response.ok) {
-			let json = await response.json();
-			console.log(json);
-			document.getElementById("notif").innerText =
-				"Recipe successfully added to favorites. ";
-		} else {
-			document.getElementById("notif").innerText =
-				"Error: Could not add recipe to favorites. ";
-		}
+    setTimeout(() => {
+      document.getElementById("notif").classList.toggle("visible");
+    }, 2500);
+  };
 
-		setTimeout(() => {
-			document.getElementById("notif").classList.toggle("visible");
-		}, 2500);
-	};
-
-	render() {
-		return (
-			<DocumentTitle title="Add A Recipe">
-				<div className="addPage">
-					<span id="notif"></span>
-					<h1>
-						<mark>Add a Recipe</mark>
-					</h1>
-					<h2>Type a food name in the box below to search for a recipe</h2>
-					<form className="searchForm" onSubmit={this.handleSearch}>
-						<input
-							type="search"
-							className="searchBar"
-							placeholder="Search for pizza, pasta, or other recipes..."
-							name="searchTerm"
-						></input>
-						<input
-							type="submit"
-							className="searchButton"
-							value="Search"
-						></input>
-					</form>
-					{this.state.loading ? (
-						<Loading />
-					) : (
-						<div className="searchResults">
-							{this.state.results.length === 0 && this.state.userHasSearched ? (
-								<p>No results found.</p>
-							) : (
-								this.state.results.map(result => {
-									return (
-										<React.Fragment key={result.recipe.url}>
-											<SearchResult
-												name={result.recipe.label}
-												author={result.recipe.source}
-												favorited={false}
-												viewDetails={() => this.viewDetails(result.recipe.url)}
-												favoriteFunction={() =>
-													this.favorite(
-														result.recipe.label,
-														result.recipe.source,
-														result.recipe.yield,
-														result.recipe.ingredientLines,
-														[],
-														result.recipe.url
-													)
-												}
-												canEdit={false}
-											/>
-											{this.state.detailView &&
-											this.state.detailLink === result.recipe.url ? (
-												<RecipeDetails
-													title={result.recipe.label}
-													author={result.recipe.source}
-													servings={result.recipe.yield}
-													ingredients={result.recipe.ingredientLines}
-													directions={[]}
-													link={result.recipe.url}
-													canEdit={false}
-												></RecipeDetails>
-											) : (
-												undefined
-											)}
-										</React.Fragment>
-									);
-								})
-							)}
-						</div>
-					)}
-				</div>
-			</DocumentTitle>
-		);
-	}
+  render() {
+    return (
+      <DocumentTitle title="Add A Recipe">
+        <div className="addPage">
+          <span id="notif"></span>
+          <h1>
+            <mark>Add a Recipe</mark>
+          </h1>
+          <h2>Type a food name in the box below to search for a recipe</h2>
+          <form className="searchForm" onSubmit={this.handleSearch}>
+            <input
+              type="search"
+              className="searchBar"
+              placeholder="Search for pizza, pasta, or other recipes..."
+              name="searchTerm"
+            ></input>
+            <input
+              type="submit"
+              className="searchButton"
+              value="Search"
+            ></input>
+          </form>
+          {this.state.loading ? (
+            <Loading />
+          ) : (
+            <div className="searchResults">
+              {this.state.results.length === 0 && this.state.userHasSearched ? (
+                <p>No results found.</p>
+              ) : (
+                this.state.results.map(result => {
+                  return (
+                    <React.Fragment key={result.recipe.url}>
+                      <SearchResult
+                        name={result.recipe.label}
+                        author={result.recipe.source}
+                        favorited={false}
+                        viewDetails={() => this.viewDetails(result.recipe.url)}
+                        favoriteFunction={() =>
+                          this.favorite(
+                            result.recipe.label,
+                            result.recipe.source,
+                            result.recipe.yield,
+                            result.recipe.ingredientLines,
+                            [],
+                            result.recipe.url
+                          )
+                        }
+                        canEdit={false}
+                      />
+                      {this.state.detailView &&
+                      this.state.detailLink === result.recipe.url ? (
+                        <RecipeDetails
+                          title={result.recipe.label}
+                          author={result.recipe.source}
+                          servings={result.recipe.yield}
+                          ingredients={result.recipe.ingredientLines}
+                          directions={[]}
+                          link={result.recipe.url}
+                          canEdit={false}
+                        ></RecipeDetails>
+                      ) : (
+                        undefined
+                      )}
+                    </React.Fragment>
+                  );
+                })
+              )}
+            </div>
+          )}
+        </div>
+      </DocumentTitle>
+    );
+  }
 }
--- src/components/BookPage.js
+++ src/components/BookPage.js
@@ -9,135 +9,135 @@ import DocumentTitle from "react-document-title";
 const API = "https://cooking-companion-api.herokuapp.com";
 
 export default class BookPage extends React.Component {
-	_isMounted = false;
+  _isMounted = false;
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			loading: false,
-			favorites: [],
-			detailLink: ""
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      favorites: [],
+      detailLink: ""
+    };
+  }
 
-	async retrieveFavorites() {
-		let url = `${API}/recipes`;
-		let response = await fetch(url);
-		let favorites = await response.json();
-		return favorites;
-	}
+  async retrieveFavorites() {
+    let url = `${API}/recipes`;
+    let response = await fetch(url);
+    let favorites = await response.json();
+    return favorites;
+  }
 
-	async componentDidMount() {
-		this._isMounted = true;
-		if (this._isMounted) {
-			this.setState({ loading: true });
-		}
-		let favorites = await this.retrieveFavorites();
-		if (this._isMounted) {
-			this.setState({ favorites, loading: false });
-		}
-	}
+  async componentDidMount() {
+    this._isMounted = true;
+    if (this._isMounted) {
+      this.setState({ loading: true });
+    }
+    let favorites = await this.retrieveFavorites();
+    if (this._isMounted) {
+      this.setState({ favorites, loading: false });
+    }
+  }
 
-	componentWillUnmount = () => {
-		this._isMounted = false;
-	};
+  componentWillUnmount = () => {
+    this._isMounted = false;
+  };
 
-	viewDetails = url => {
-		if (this._isMounted) {
-			url === this.state.detailLink
-				? this.setState({ detailView: false, detailLink: "" })
-				: this.setState({ detailView: true, detailLink: url });
-		}
-	};
+  viewDetails = url => {
+    if (this._isMounted) {
+      url === this.state.detailLink
+        ? this.setState({ detailView: false, detailLink: "" })
+        : this.setState({ detailView: true, detailLink: url });
+    }
+  };
 
-	unfavorite = async (title, author) => {
-		let url = `${API}/recipes/${author}/${title}`;
-		const response = await fetch(url, { method: "DELETE" });
-		if (response.ok) {
-			this.showNotification("Recipe successfully removed from favorites.");
-			this.reloadFavorites();
-		} else {
-			this.showNotification("Error: Could not unfavorite this recipe.");
-		}
-	};
+  unfavorite = async (title, author) => {
+    let url = `${API}/recipes/${author}/${title}`;
+    const response = await fetch(url, { method: "DELETE" });
+    if (response.ok) {
+      this.showNotification("Recipe successfully removed from favorites.");
+      this.reloadFavorites();
+    } else {
+      this.showNotification("Error: Could not unfavorite this recipe.");
+    }
+  };
 
-	reloadFavorites = async () => {
-		let favorites = await this.retrieveFavorites();
-		if (this._isMounted) {
-			this.setState({ favorites });
-		}
-	};
+  reloadFavorites = async () => {
+    let favorites = await this.retrieveFavorites();
+    if (this._isMounted) {
+      this.setState({ favorites });
+    }
+  };
 
-	showNotification = message => {
-		document.getElementById("notif").classList.toggle("visible");
-		document.getElementById("notif").innerText = message;
-		setTimeout(() => {
-			document.getElementById("notif").classList.toggle("visible");
-		}, 2500);
-	};
+  showNotification = message => {
+    document.getElementById("notif").classList.toggle("visible");
+    document.getElementById("notif").innerText = message;
+    setTimeout(() => {
+      document.getElementById("notif").classList.toggle("visible");
+    }, 2500);
+  };
 
-	render() {
-		return (
-			<DocumentTitle title="Recipe Book">
-				<div className="bookPage">
-					<span id="notif"></span>
+  render() {
+    return (
+      <DocumentTitle title="Recipe Book">
+        <div className="bookPage">
+          <span id="notif"></span>
 
-					<h1>
-						<mark>Recipe Book</mark>
-					</h1>
-					<h2>
-						Browse through your saved recipes, edit recipes, or delete recipes
-					</h2>
+          <h1>
+            <mark>Recipe Book</mark>
+          </h1>
+          <h2>
+            Browse through your saved recipes, edit recipes, or delete recipes
+          </h2>
 
-					{this.state.loading ? (
-						<Loading />
-					) : (
-						<div className="searchResults">
-							{this.state.favorites.length === 0 ? (
-								<p data-testid="no-book-results">
-									No favorites found. Add some recipes to your recipe book using
-									the Add Recipe page.
-								</p>
-							) : (
-								<div data-testid="favorited-recipes">
-									{this.state.favorites.map(favorite => {
-										return (
-											<React.Fragment key={favorite.url}>
-												<SearchResult
-													name={favorite.title}
-													author={favorite.author}
-													favorited={true}
-													viewDetails={() => this.viewDetails(favorite.url)}
-													favoriteFunction={() =>
-														this.unfavorite(favorite.title, favorite.author)
-													}
-													canEdit={true}
-													showNotification={this.showNotification}
-													reloadFavorites={this.reloadFavorites}
-												/>
-												{this.state.detailView &&
-												this.state.detailLink === favorite.url ? (
-													<RecipeDetails
-														title={favorite.title}
-														author={favorite.author}
-														servings={favorite.servings}
-														ingredients={favorite.ingredients}
-														directions={favorite.directions}
-														link={favorite.url}
-														canEdit={true}
-													/>
-												) : (
-													undefined
-												)}
-											</React.Fragment>
-										);
-									})}
-								</div>
-							)}
-						</div>
-					)}
-				</div>
-			</DocumentTitle>
-		);
-	}
+          {this.state.loading ? (
+            <Loading />
+          ) : (
+            <div className="searchResults">
+              {this.state.favorites.length === 0 ? (
+                <p data-testid="no-book-results">
+                  No favorites found. Add some recipes to your recipe book using
+                  the Add Recipe page.
+                </p>
+              ) : (
+                <div data-testid="favorited-recipes">
+                  {this.state.favorites.map(favorite => {
+                    return (
+                      <React.Fragment key={favorite.url}>
+                        <SearchResult
+                          name={favorite.title}
+                          author={favorite.author}
+                          favorited={true}
+                          viewDetails={() => this.viewDetails(favorite.url)}
+                          favoriteFunction={() =>
+                            this.unfavorite(favorite.title, favorite.author)
+                          }
+                          canEdit={true}
+                          showNotification={this.showNotification}
+                          reloadFavorites={this.reloadFavorites}
+                        />
+                        {this.state.detailView &&
+                        this.state.detailLink === favorite.url ? (
+                          <RecipeDetails
+                            title={favorite.title}
+                            author={favorite.author}
+                            servings={favorite.servings}
+                            ingredients={favorite.ingredients}
+                            directions={favorite.directions}
+                            link={favorite.url}
+                            canEdit={true}
+                          />
+                        ) : (
+                          undefined
+                        )}
+                      </React.Fragment>
+                    );
+                  })}
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+      </DocumentTitle>
+    );
+  }
 }
